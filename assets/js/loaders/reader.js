5b3dc05e328d860cecde6040c91d4a69c0962ca1
azurenekowo <sakurazumii.5792@gmail.com>
2024-01-05 21:10:12 +0700

    UI fixes - /read/ demo

---
+++ assets/js/loaders/reader.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', async () => {
+    // await new Promise(resolve => setTimeout(resolve, 1000))
+    document.querySelector('.navbar').classList.add('charlotte-hidden')
+    document.querySelector('#messageDialog').classList.add('charlotte-hidden')
+
+    document.querySelector('.doujin-reader').classList.remove('d-none')
+})
